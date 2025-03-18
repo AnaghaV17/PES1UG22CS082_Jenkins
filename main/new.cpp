5783bbc5c3faba20f4ed2d72acc3c1adbf15fd7d
Anagha V <anaghav@Anaghas-MacBook-Air-2.local>
2025-03-18 16:35:49 +0530

    Task 4

---
+++ main/new.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hey there!\n";
+  cout << "Hello, PES1UG22CS082\n";
+  cout << "This is my new cpp file  \n";
+  return 0;
+}
