ccdd39f245a3cf9ae1375650e0f25301b11a4ad3
Anagha V <anaghav@Anaghas-MacBook-Air-2.local>
2025-03-18 16:02:17 +0530

    Task 4

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, Anagha\n";
+  cout << "Hello, Anagha V\n";
   cout << "Hello, PES1UG22CS082\n";
   cout << "I have successfully built and run this  \n";
   return 0;
